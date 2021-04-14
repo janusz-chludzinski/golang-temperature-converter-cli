c03f2059a92b7a9dfc6153fc45d90f4486204dfe
Janusz Chludziński <78743486+sfc-gh-jchludzinski@users.noreply.github.com>
2021-04-14 18:29:42 +0200

    second task

--- main.go
+++ main.go
@@ -25,10 +25,20 @@ func main() {
 
 	for {
 		fmt.Print("What is the current temperature in " + originUnit + " ? ")
-
+		_, err = fmt.Scanln(&originValue)
+		if err != nil {
+			printError(errReadingInput)
+		} else {
+			convertToCelsius(originValue)
+		}
 		fmt.Print("Would you like to convert another temperature ? (y/n) ")
+		_, err = fmt.Scanln(&shouldConvertAgain)
 
-		if shouldConvertAgain != "Y" {
+		if err != nil {
+			printError(errReadingInput)
+		}
+
+		if strings.ToUpper(strings.TrimSpace(shouldConvertAgain)) != "Y" {
 			fmt.Println("Good bye!")
 			break
 		}
